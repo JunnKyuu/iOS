ae2d4d5160b2aefcaa869e30a5f4573177bea898
JunnKyuu <junnkyuu22@gmail.com>
2023-02-26 23:16:30 +0900

    SymbolRoller V1

---
+++ SymbolRoller/SymbolRoller/SymbolRollerViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SymbolRollerViewController.swift
+//  SymbolRoller
+//
+//  Created by JunKyu Lee on 2023/02/26.
+//
+
+import UIKit
+
+class SymbolRollerViewController: UIViewController {
+    
+    let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // To-Do
+        // - 심볼에서 하나를 임의로 추출해서,
+        // 이미지와 텍스트를 설정한다.
+        
+        let symbol = symbols.randomElement()!
+        imageView.image = UIImage(systemName: symbol)
+        label.text = symbol
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("Tapped!!")
+        let symbol = symbols.randomElement()!
+        imageView.image = UIImage(systemName: symbol)
+        label.text = symbol
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
