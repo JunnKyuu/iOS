19440d513a2e27841ee527a52dd780aacddb0901
JunnKyuu <junnkyuu22@gmail.com>
2023-03-01 12:40:56 +0900

    V2 - button 클릭하면 random으로 cityLabel, weatherImage, tempLabel 변경 기능

--- SimpleWeather/SimpleWeather/WeatherViewController.swift
+++ SimpleWeather/SimpleWeather/WeatherViewController.swift
@@ -8,22 +8,33 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    @IBOutlet var weatherImageView: UIImageView!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    let cities: [String] = ["Seoul", "Tokyo", "LA", "Seattle"]
+    
+    let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        print("도시, 온도, 날씨 이미지 변경하자!!")
+        
+        cityLabel.text = cities.randomElement()!
+        let imageName = weathers.randomElement()!
+        weatherImageView.image = UIImage(systemName: imageName)!.withRenderingMode(.alwaysOriginal)
+        
+        let randomTemp = Int.random(in: 10..<30)
+        temperatureLabel.text = "\(randomTemp)°"
+        
+        let randomCity = cities.randomElement()!
+        cityLabel.text = randomCity
     }
-    */
+    
 
 }
