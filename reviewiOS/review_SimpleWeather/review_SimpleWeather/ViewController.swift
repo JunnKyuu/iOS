160ab90b829fd15240495c3174cceb38f66a7d9e
JunnKyuu <junnkyuu22@gmail.com>
2023-03-04 23:24:06 +0900

    simple weather - stackView를 통한 Auto Layout 및 View 구성 복습

---
+++ reviewiOS/review_SimpleWeather/review_SimpleWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  review_SimpleWeather
+//
+//  Created by JunKyu Lee on 2023/03/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ reviewiOS/review_SimpleWeather/review_SimpleWeatherUITests/review_SimpleWeatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  review_SimpleWeatherUITestsLaunchTests.swift
+//  review_SimpleWeatherUITests
+//
+//  Created by JunKyu Lee on 2023/03/04.
+//
+
+import XCTest
+
+final class review_SimpleWeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
