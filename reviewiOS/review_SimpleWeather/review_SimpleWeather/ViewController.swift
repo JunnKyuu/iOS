e42a450b378694008dd8c7dc02f14974f0576c20
JunnKyuu <junnkyuu22@gmail.com>
2023-03-05 15:08:49 +0900

    랜덤으로 label, imageView 나오는 기능 추가 복습

--- reviewiOS/review_SimpleWeather/review_SimpleWeather/ViewController.swift
+++ reviewiOS/review_SimpleWeather/review_SimpleWeather/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var weatherImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    let cities = ["Seoul", "Seattle", "LA", "Tokyo", "Osaka"]
+    let weathers = ["sun.max.fill", "wind", "cloud.fill", "cloud.bolt.fill", "cloud.bolt.fill"]
+    
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        cityLabel.text = cities.randomElement()!
+        
+        let imageView = weathers.randomElement()!
+        let temp = Int.random(in: 10..<30)
+        
+        temperatureLabel.text = "\(temp)°"
+        
+        weatherImageView.image = UIImage(systemName: imageView)?.withRenderingMode(.alwaysOriginal)
+    }
 }
-
