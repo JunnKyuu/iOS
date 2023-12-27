1d72a8170ee5bca34d4e4a918e25f0c98bfdc726
JunnKyuu <junnkyuu22@gmail.com>
2023-12-27 15:04:16 +0900

    UIKit - Storyboard를 이용한 simpleweather

--- SimpleWeather/SimpleWeather/WeatherViewController.swift
+++ SimpleWeather/SimpleWeather/WeatherViewController.swift
@@ -8,31 +8,7 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-    @IBOutlet var weatherImageView: UIImageView!
-    @IBOutlet var cityLabel: UILabel!
-    @IBOutlet var temperatureLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    let cities: [String] = ["서울", "수원", "용인", "부산"]
-    
-    let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
-
-    @IBAction func changeButtonTapped(_ sender: Any) {
-        print("도시, 온도, 날씨 이미지 변경")
-        
-        cityLabel.text = cities.randomElement()!
-        let imageName = weathers.randomElement()!
-        weatherImageView.image = UIImage(systemName: imageName)!.withRenderingMode(.alwaysOriginal)
-        
-        let randomTemp = Int.random(in: -15..<3)
-        temperatureLabel.text = "\(randomTemp)°"
-        
-        let randomCity = cities.randomElement()!
-        cityLabel.text = randomCity
-    }
-    
-
 }
