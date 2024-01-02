27bcadb8bd745c044d40deee78854fe9e81c1237
JunnKyuu <junnkyuu22@gmail.com>
2024-01-02 17:23:19 +0900

    회원가입, 로그인, 로그아웃 로직 구현

--- LoginSampleApp/LoginSampleApp/EnterEmailViewController.swift
+++ LoginSampleApp/LoginSampleApp/EnterEmailViewController.swift
@@ -49,7 +49,17 @@ class EnterEmailViewController: UIViewController {
         Auth.auth().createUser(withEmail:  email, password:  password) { [weak self] authResult, error in
             guard let self = self else { return }
             
-            self.showMainViewController()
+            if let error = error {
+                let code = (error as NSError).code
+                switch code {
+                case 17007:  // 이미 가입한 계정일 때
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    self.errorMessageLabel.text = error.localizedDescription
+                }
+            } else {
+                self.showMainViewController()
+            }
         }
     }
     
@@ -58,6 +68,18 @@ class EnterEmailViewController: UIViewController {
         let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
         mainViewController.modalPresentationStyle = .fullScreen
         navigationController?.show(mainViewController, sender:  nil)
+    }
+    
+    private func loginUser(withEmail email: String,  password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] _, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                self.errorMessageLabel.text = error.localizedDescription
+            } else {
+                self.showMainViewController()
+            }
+        }
     }
 }
 
--- LoginSampleApp/LoginSampleApp/MainViewController.swift
+++ LoginSampleApp/LoginSampleApp/MainViewController.swift
@@ -17,8 +17,8 @@ class MainViewController: UIViewController {
         
         // MainView로 왔을 때 뒤로가기하면 어색하므로 막아둠
         navigationController?.interactivePopGestureRecognizer?.isEnabled = false
-        
-        logoutButton.layer.cornerRadius = 30
+    
+        logoutButton.layer.cornerRadius = 10
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,6 +36,13 @@ class MainViewController: UIViewController {
     }
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
         // 버튼 클릭하면 첫 화면으로 넘어가기
-        self.navigationController?.popToRootViewController(animated: true)
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error: signOut \(signOutError.localizedDescription)")
+        }
     }
 }
