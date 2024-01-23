945816e225b87a3aa8be8955e4aa795325d8bc2a
JunnKyuu <junnkyuu22@gmail.com>
2024-01-23 17:22:35 +0900

    AssetMenuData imageName, title 데이터 추가

--- MyAssets/MyAssets/AssetMenu.swift
+++ MyAssets/MyAssets/AssetMenu.swift
@@ -20,4 +20,46 @@ enum AssetMenu: String, Identifiable, Decodable {
     var id: String {
         return self.rawValue
     }
+    
+    var systemImageName: String {
+        switch self {
+        case .creditScore:
+            return "number.circle"
+        case .account:
+            return "banknote"
+        case .investment:
+            return "bitcoinsign.circle"
+        case .loan:
+            return "hand.wave"
+        case .insurance:
+            return "lock.shield"
+        case .creditCard:
+            return "creditcard"
+        case .cash:
+            return "dollarsign.circle"
+        case .realEstate:
+            return "house.fill"
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .creditScore:
+            return "신용점수"
+        case .account:
+            return "계좌"
+        case .investment:
+            return "투지"
+        case .loan:
+            return "대출"
+        case .insurance:
+            return "보험"
+        case .creditCard:
+            return "카드"
+        case .cash:
+            return "현금"
+        case .realEstate:
+            return "부동산"
+        }
+    }
 }
