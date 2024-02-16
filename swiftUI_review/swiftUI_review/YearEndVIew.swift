96ae2d843869af99988802af41ca07ab8053e9f7
JunnKyuu <junnkyuu22@gmail.com>
2024-02-16 17:34:07 +0900

    연말정산 서류 간편 발급 Stack 구현

--- swiftUI_review/swiftUI_review/YearEndVIew.swift
+++ swiftUI_review/swiftUI_review/YearEndVIew.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct YearEndVIew: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 10) {
 //            연말정산 확인하기
             VStack(spacing: 30) {
                 HStack(spacing: 10) {
@@ -20,7 +20,6 @@ struct YearEndVIew: View {
                         Text("13월의 월급 확인하기")
                             .font(.system(size: 15, weight: .light, design: .default))
                             .foregroundColor(Color.gray)
-
                     }
                     Spacer()
                     Image(systemName: "calendar")
@@ -29,7 +28,7 @@ struct YearEndVIew: View {
                         .frame(width: 80, height: 80)
                         .foregroundColor(Color.white)
                 }
-                .padding(20)
+                .padding(5)
                 .frame(maxWidth: .infinity)
                 Button(action: {
                     print("Button Tapped!")
@@ -39,15 +38,56 @@ struct YearEndVIew: View {
                         .foregroundColor(Color.white)
                 })
                 .frame(maxWidth: .infinity)
-                .padding(20)
+                .padding(15)
                 .background(Color.blue)
                 .cornerRadius(10)
             }
             .padding(20)
-            .background(Color.black)
+            .background(Color(hex: "#010240"))
+            .cornerRadius(10)
+//            연말정산 서류 간편 발금
+            VStack {
+                HStack(spacing: 30) {
+                    VStack(alignment: .leading) {
+                        Text("연말정산 서류 간편 발급")
+                            .font(.system(size: 18, weight: .bold, design: .default))
+                            .foregroundColor(.white)
+                        Text("3분만에 자료 보내기")
+                            .font(.system(size: 15, weight: .light, design: .default))
+                            .foregroundColor(Color(hex: "#B0BFFF"))
+                    }
+                    Spacer()
+                    Image(systemName: "paperplane.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color(hex: "#FFFFFF"))
+                }
+                .padding(20)
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color(hex: "#1C46B5"))
+            .cornerRadius(10)
         }
         .cornerRadius(10)
         .padding(20)
+    }
+}
+
+// hex color extension
+extension Color {
+    init(hex: String) {
+        var cleanHexCode = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        cleanHexCode = cleanHexCode.replacingOccurrences(of: "#", with: "")
+        print(cleanHexCode)
+        var rgb: UInt64 = 0
+        
+        Scanner(string: cleanHexCode).scanHexInt64(&rgb)
+        
+        let redValue = Double((rgb >> 16) & 0xFF) / 255.0
+        let greenValue = Double((rgb >> 8) & 0xFF) / 255.0
+        let blueValue = Double(rgb & 0xFF) / 255.0
+        self.init(red: redValue, green: greenValue, blue: blueValue)
     }
 }
 
