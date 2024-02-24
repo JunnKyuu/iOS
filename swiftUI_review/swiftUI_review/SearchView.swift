4ef0ae46365b8dc5c67b8d138a72210756e2e361
JunnKyuu <junnkyuu22@gmail.com>
2024-02-25 01:56:21 +0900

    SearchView header 추가

--- swiftUI_review/swiftUI_review/SearchView.swift
+++ swiftUI_review/swiftUI_review/SearchView.swift
@@ -10,7 +10,22 @@ import SwiftUI
 struct SearchView: View {
     var body: some View {
         ScrollView {
-         Text("돈 버는 서비스")
+            VStack {
+                HStack {
+                    Text("돈 버는 서비스")
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                    Spacer()
+                    Text("새 서비스 알림 받기")
+                        .font(.system(size: 16, weight: .medium, design: .default))
+                        .foregroundColor(Color(hex: "#000000"))
+                        .padding(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color(hex: "dddddd").opacity(0.5))
+                        )
+                }
+                .padding()
+            }
         }
     }
 }
