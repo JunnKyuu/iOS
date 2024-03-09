e370aa4d27e9d9aeeb044a0d4749dc3d685ed208
JunnKyuu <junnkyuu22@gmail.com>
2024-03-10 02:52:39 +0900

    Refund Detail View 홈으로 돌아가기 버튼 수정

--- swiftUI_review/swiftUI_review/Views/RefundDetailView.swift
+++ swiftUI_review/swiftUI_review/Views/RefundDetailView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct RefundDetailView: View {
     @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         Button(action: {
             dismiss()
         }, label: {
-            Text("뒤로가기")
+            Text("홈으로 돌아가기")
+                .font(.system(size: 18, weight: .bold, design: .default))
+                .foregroundColor(Color(hex: "#000000"))
         })
+        .padding()
+        .background(Color(hex: "#dddddd"))
+        .cornerRadius(10)
     }
 }
 
